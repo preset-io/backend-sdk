9306564b0768c4aa71ef655c11f9781e860ec7f8
Beto Dealmeida <roberto@dealmeida.net>
2022-01-26 14:57:30 -0800

    WIP

---
+++ src/preset_cli/api/client.py
@@ -0,0 +1,50 @@
+"""
+A simple client for interacting with the Preset API.
+"""
+
+from typing import Any, List, Union
+
+from superset_sdk.auth.main import Auth
+from yarl import URL
+
+
+class PresetClient:  # pylint: disable=too-few-public-methods
+
+    """
+    A client for the Preset API.
+    """
+
+    def __init__(self, baseurl: Union[str, URL], auth: Auth):
+        # convert to URL if necessary
+        self.baseurl = URL(baseurl)
+        self.auth = auth
+
+    def get_teams(self) -> List[Any]:
+        """
+        Retrieve all teams based on membership.
+        """
+        session = self.auth.get_session()
+        headers = self.auth.get_headers()
+        response = session.get(self.baseurl / "api/v1/teams/", headers=headers)
+        response.raise_for_status()
+
+        payload = response.json()
+        teams = payload["payload"]
+
+        return teams
+
+    def get_workspaces(self, team_name: str) -> List[Any]:
+        """
+        Retrieve all workspaces for a given team.
+        """
+        session = self.auth.get_session()
+        headers = self.auth.get_headers()
+        response = session.get(
+            self.baseurl / "api/v1/teams" / team_name / "workspaces/", headers=headers
+        )
+        response.raise_for_status()
+
+        payload = response.json()
+        workspaces = payload["payload"]
+
+        return workspaces
--- src/preset_cli/cli/superset/main.py
+++ src/preset_cli/cli/superset/main.py
@@ -10,11 +10,20 @@ from superset_sdk.auth.jwt import JWTAuth
 from superset_sdk.cli.main import superset_cli
 from yarl import URL
 
+from preset_cli.api.client import PresetClient
 
-def split_comma(ctx: click.core.Context, param: str, value: str) -> List[str]:
+
+def split_comma(  # pylint: disable=unused-argument
+    ctx: click.core.Context,
+    param: str,
+    value: Optional[str],
+) -> List[str]:
     """
     Split CLI option into multiple values.
     """
+    if value is None:
+        return []
+
     return [option.strip() for option in value.split(",")]
 
 
@@ -29,6 +38,53 @@ def get_access_token(baseurl: URL, api_token: str, api_secret: str) -> str:
     )
     payload = response.json()
     return payload["payload"]["access_token"]
+
+
+def get_status_icon(status: str) -> str:
+    """
+    Return an icon (emoji) for a given status.
+    """
+    print(status)
+    icons = {
+        "READY": "✅",
+        "LOADING_EXAMPLES": "📊",
+        "CREATING_DB": "💾",
+        "INITIALIZING_DB": "💾",
+        "MIGRATING_DB": "🚧",
+        "ROTATING_SECRETS": "🕵️",
+        "UNKNOWN": "❓",
+        "ERROR": "❗️",
+        "UPGRADING": "⤴️",
+    }
+    return icons.get(status, "❓")
+
+
+def parse_workspace_selection(selection: str, count: int) -> List[int]:
+    """
+    Parse a range of numbers.
+
+        >>> parse_workspace_selection("1-4,7", 10)
+        [1, 2, 3, 4, 7]
+
+    """
+    numbers = []
+    for part in selection.split(","):
+        if "-" in part:
+            if part[0] == "-":
+                part = "1" + part
+            if part[-1] == "-":
+                part = part + str(count)
+            start, end = [int(number) for number in part.split("-", 1)]
+            if end > count:
+                raise Exception("Range {part} is greater than {count}")
+            numbers.extend(range(start, end + 1))
+        else:
+            number = int(part)
+            if number > count:
+                raise Exception("Number {number} is greater than {count}")
+            numbers.append(int(part))
+
+    return numbers
 
 
 @click.group()
@@ -48,16 +104,38 @@ def superset(
     Send commands to one or more Superset instances.
     """
     ctx.ensure_object(dict)
-    # XXX map from workspace name to URLs
-    # XXX prompt for workspaces
 
-    # Store workspaces, since the Superset command will run for each workspace, instead
-    # of a single instance.
-    ctx.obj["WORKSPACES"] = workspaces
-
-    # Store auth.
+    # store auth so it's used by the Superset SDK
     access_token = get_access_token(URL(baseurl), api_token, api_secret)
     ctx.obj["AUTH"] = JWTAuth(access_token)
+
+    if not workspaces:
+        client = PresetClient(baseurl, ctx.obj["AUTH"])
+        click.echo("Choose one or more workspaces (eg: 1-3,5,8-):")
+        i = 1
+        hostnames = {}
+        for team in client.get_teams():
+            click.echo(f'\n# {team["title"]} #')
+            for workspace in client.get_workspaces(team_name=team["name"]):
+                status = get_status_icon(workspace["workspace_status"])
+                click.echo(f'{status} ({i}) {workspace["title"]}')
+                hostnames[i] = "https://" + workspace["hostname"]
+                i += 1
+
+        if i == 1:
+            click.echo("No workspaces available")
+            return
+
+        while not workspaces:
+            try:
+                choices = parse_workspace_selection(input("> "), i - 1)
+                workspaces = [hostnames[choice] for choice in choices]
+                break
+            except Exception:  # pylint: disable=broad-except
+                click.echo("Invalid choice")
+
+    # store workspaces in order to invoke the command for each one
+    ctx.obj["WORKSPACES"] = workspaces
 
 
 def add_superset_commands(group: click.core.Group) -> None:
@@ -68,8 +146,11 @@ def add_superset_commands(group: click.core.Group) -> None:
 
         @click.command()
         @click.pass_context
-        def new_command(ctx: click.core.Context, *args: Any, **kwargs: Any) -> None:
+        def new_command(
+            ctx: click.core.Context, *args: Any, command=command, **kwargs: Any
+        ) -> None:
             for instance in ctx.obj["WORKSPACES"]:
+                click.echo(f"\n{instance}")
                 ctx.invoke(command, instance=instance, *args, **kwargs)
 
         # remove INSTANCE argument from the command
