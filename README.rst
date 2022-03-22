041856f64193dd23ee5c8c465a102e6de5fc46fa
Beto Dealmeida <roberto@dealmeida.net>
2022-03-21 18:48:50 -0700

    Add auth command

--- README.rst
+++ README.rst
@@ -13,7 +13,61 @@ Install the CLI with ``pip``:
 
 .. code-block:: bash
 
-    $ pip install -U setuptools setuptools_scm wheel
-    $ pip install "git+https://github.com/preset-io/preset-cli.git#egg=preset-cli"
+    $ pip install -U setuptools setuptools_scm wheel  # for good measure
+    $ pip install "git+https://github.com/preset-io/preset-cli.git"
+
+Make sure you're using Python 3.8 or newer.
+
+Authentication
+==============
+
+The CLI requires an API key for authentication, composed of token and an associated secret. Both can be defined in your environment as ``PRESET_API_TOKEN`` and ``PRESET_API_SECRET``, respectively, or can be stored in a configuration file. To store the credentials in a configuration file simply run ``preset-cli auth``. This should open https://manage.app.preset.io/app/user in your browser so you can generate a new token, and it will prompt you for the token and its secret:
+
+.. code-block:: bash
+
+    % preset-cli auth
+    Please generate a new token at https://manage.app.preset.io/app/user if you don't have one already
+    API token: 35dac901-c775-43ff-8eb4-816edc061487
+    API secret: [will not echo]
+    Credentials stored in /Users/beto/Library/Application Support/preset-cli/credentials.yaml
+
+The credentials will be stored in a system-dependent location, in a file that's readable only by you.
+
+This step is optional. If you try to run the CLI without the token/secret defined in your environment or stored in the expected location you will be prompted for them:
+
+.. code-block:: bash
+
+    % preset-cli superset sql
+    You need to specify a JWT token or an API key (name and secret)
+    API token: 35dac901-c775-43ff-8eb4-816edc061487
+    API secret: [will not echo]
+    Store the credentials in /Users/beto/Library/Application Support/preset-cli/credentials.yaml? [y/N]
+
+You can also pass the token/secret (or even the JWT token) directly as an option:
+
+.. code-block:: bash
+
+    % preset-cli --api-token 35dac901-c775-43ff-8eb4-816edc061487 --api-secret XXX superset sql
+    % preset-cli --jwt-token XXX superset sql
+
+In summary, there are 3 options for credentials:
+
+1. Stored in the environment as ``PRESET_API_TOKEN`` and ``PRESET_API_SECRET``;
+2. Stored in a user-readable file called ``credentials.yaml``, with system-dependent location;
+3. Passed directly to the CLI via ``--api-token`` and ``--api-secret`` (or ``--jwt-token``) options.
+
+Workspaces
+==========
 
 
+Commands
+========
+
+Running SQL
+-----------
+
+Synchronizing from exports
+--------------------------
+
+Synchronizing to and from DBT
+-----------------------------
--- src/preset_cli/cli/main.py
+++ src/preset_cli/cli/main.py
@@ -3,6 +3,7 @@ Main entry point for the CLI.
 """
 
 import getpass
+import sys
 import webbrowser
 from pathlib import Path
 from typing import List, Optional
@@ -93,6 +94,32 @@ def get_access_token(baseurl: URL, api_token: str, api_secret: str) -> str:
     return payload["payload"]["access_token"]
 
 
+def get_credentials_path() -> Path:
+    """
+    Return the system-dependent location of the credentials.
+    """
+    config_dir = Path(user_config_dir("preset-cli", "Preset"))
+    return config_dir / CREDENTIALS_FILE
+
+
+def store_credentials(api_token: str, api_secret: str, credentials_path: Path) -> None:
+    """
+    Store credentials.
+    """
+    credentials_path.parent.mkdir(parents=True, exist_ok=True)
+
+    while True:
+        store = input(f"Store the credentials in {credentials_path}? [y/N] ")
+        if store.strip().lower() == "y":
+            with open(credentials_path, "w", encoding="utf-8") as output:
+                yaml.safe_dump(dict(api_token=api_token, api_secret=api_secret), output)
+            credentials_path.chmod(0o600)
+            break
+
+        if store.strip().lower() in ("n", ""):
+            break
+
+
 @click.group()
 @click.option("--baseurl", default="https://manage.app.preset.io/")
 @click.option("--api-token", envvar="PRESET_API_TOKEN")
@@ -119,9 +146,7 @@ def preset_cli(  # pylint: disable=too-many-branches, too-many-locals, too-many-
     if jwt_token is None:
         if api_token is None or api_secret is None:
             # check for stored credentials
-            config_dir = Path(user_config_dir("preset-cli", "Preset"))
-            config_dir.mkdir(parents=True, exist_ok=True)
-            credentials_path = config_dir / CREDENTIALS_FILE
+            credentials_path = get_credentials_path()
             if credentials_path.exists():
                 with open(credentials_path, encoding="utf-8") as input_:
                     credentials = yaml.load(input_, Loader=yaml.SafeLoader)
@@ -132,30 +157,16 @@ def preset_cli(  # pylint: disable=too-many-branches, too-many-locals, too-many-
                     "You need to specify a JWT token or an API key (name and secret)"
                 )
                 webbrowser.open(str(manager_url / "app/user"))
-
-                # get the credentials from the user
                 api_token = input("API token: ")
                 api_secret = getpass.getpass("API secret: ")
-
-                while True:
-                    store = input(f"Store the credentials in {config_dir}? [y/N] ")
-                    if store.strip().lower() == "y":
-                        with open(credentials_path, "w", encoding="utf-8") as output:
-                            yaml.safe_dump(
-                                dict(api_token=api_token, api_secret=api_secret), output
-                            )
-                        credentials_path.chmod(0o600)
-                        break
-
-                    if store.strip().lower() in ("n", ""):
-                        break
+                store_credentials(api_token, api_secret, credentials_path)
 
         jwt_token = get_access_token(manager_url, api_token, api_secret)
 
     # store auth in context so it's used by the Superset SDK
     ctx.obj["AUTH"] = JWTAuth(jwt_token)
 
-    if not workspaces:
+    if not workspaces and ctx.invoked_subcommand == "superset":
         client = PresetClient(ctx.obj["MANAGER_URL"], ctx.obj["AUTH"])
         click.echo("Choose one or more workspaces (eg: 1-3,5,8-):")
         i = 1
@@ -184,4 +195,39 @@ def preset_cli(  # pylint: disable=too-many-branches, too-many-locals, too-many-
     ctx.obj["WORKSPACES"] = workspaces
 
 
+@click.command()
+@click.option("--baseurl", default="https://manage.app.preset.io/")
+@click.option(
+    "--overwrite", is_flag=True, default=False, help="Overwrite existing credentials"
+)
+def auth(baseurl: str, overwrite: bool = False) -> None:
+    """
+    Store credentials for auth.
+    """
+    credentials_path = get_credentials_path()
+    if credentials_path.exists() and not overwrite:
+        click.echo(
+            click.style(
+                (
+                    f"The file {credentials_path} already exists. "
+                    "Pass --overwrite to replace it."
+                ),
+                fg="bright_red",
+            )
+        )
+        sys.exit(1)
+
+    manager_url = URL(baseurl)
+    click.echo(
+        f"Please generate a new token at {manager_url} if you don't have one already"
+    )
+    webbrowser.open(str(manager_url / "app/user"))
+    api_token = input("API token: ")
+    api_secret = getpass.getpass("API secret: ")
+
+    store_credentials(api_token, api_secret, credentials_path)
+    click.echo(f"Credentials stored in {credentials_path}")
+
+
+preset_cli.add_command(auth)
 preset_cli.add_command(superset)
