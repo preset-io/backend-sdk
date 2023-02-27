aa365ce838a367c257fb09c55dbab47891bff8f6
Beto Dealmeida <roberto@dealmeida.net>
2023-02-27 11:44:50 -0800

    fix: use table alias when creating dataset

--- tests/cli/superset/sync/dbt/datasets_test.py
+++ tests/cli/superset/sync/dbt/datasets_test.py
@@ -127,6 +127,96 @@ def test_sync_datasets_new(mocker: MockerFixture) -> None:
     )
 
 
+def test_sync_datasets_with_alias(mocker: MockerFixture) -> None:
+    """
+    Test ``sync_datasets`` when datasets has an alias.
+    """
+    client = mocker.MagicMock()
+    client.get_datasets.return_value = []
+    client.create_dataset.side_effect = [{"id": 1}, {"id": 2}, {"id": 3}]
+    client.get_dataset.return_value = {
+        "columns": [
+            {"column_name": "id", "is_dttm": False, "type_generic": "INTEGER"},
+            {"column_name": "ts", "is_dttm": True, "type_generic": "TIMESTAMP"},
+        ],
+    }
+
+    models_with_alias: List[ModelSchema] = [
+        model_schema.load(
+            {
+                "alias": "model_alias",
+                "database": "examples_dev",
+                "schema": "public",
+                "description": "",
+                "meta": {},
+                "name": "messages_channels",
+                "unique_id": "model.superset_examples.messages_channels",
+                "columns": {"id": {"description": "Primary key"}},
+            },
+        ),
+    ]
+    sync_datasets(
+        client=client,
+        models=models_with_alias,
+        metrics=metrics,
+        database={"id": 1, "sqlalchemy_uri": "postgresql://user@host/examples_dev"},
+        disallow_edits=False,
+        external_url_prefix="",
+    )
+    client.create_dataset.assert_has_calls(
+        [
+            mock.call(database=1, schema="public", table_name="model_alias"),
+        ],
+    )
+    client.update_dataset.assert_has_calls(
+        [
+            mock.call(
+                1,
+                override_columns=True,
+                description="",
+                extra=json.dumps(
+                    {
+                        "unique_id": "model.superset_examples.messages_channels",
+                        "depends_on": "ref('messages_channels')",
+                        "certification": {"details": "This table is produced by dbt"},
+                    },
+                ),
+                is_managed_externally=False,
+                metrics=[],
+            ),
+            mock.call(
+                1,
+                override_columns=False,
+                metrics=[
+                    {
+                        "expression": "COUNT(*)",
+                        "metric_name": "cnt",
+                        "metric_type": "count",
+                        "verbose_name": "",
+                        "description": "",
+                        "extra": "{}",
+                    },
+                ],
+            ),
+            mock.call(
+                1,
+                override_columns=True,
+                columns=[
+                    {
+                        "column_name": "id",
+                        "description": "Primary key",
+                        "is_dttm": False,
+                    },
+                    {
+                        "column_name": "ts",
+                        "is_dttm": True,
+                    },
+                ],
+            ),
+        ],
+    )
+
+
 def test_sync_datasets_no_metrics(mocker: MockerFixture) -> None:
     """
     Test ``sync_datasets`` when no datasets exist yet.
